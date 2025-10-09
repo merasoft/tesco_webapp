0866a10eb532570a48264796c2dca3c153a59c95
Usmon Rustamov <u.umarbekovich@gmail.com>
2025-10-09 18:45:49 +0500

    add adress

--- src/app/components/address-drawer/address-drawer.component.ts
+++ src/app/components/address-drawer/address-drawer.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, OnChanges, SimpleChanges, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { DataService } from '../../services/data.service';
 
 export interface Address {
@@ -16,7 +17,7 @@ export interface Address {
   templateUrl: './address-drawer.component.html',
   styleUrls: ['./address-drawer.component.scss'],
 })
-export class AddressDrawerComponent implements OnInit {
+export class AddressDrawerComponent implements OnInit, OnChanges {
   @Input() visible = false;
   @Input() initialAddress?: Address;
   @Output() visibleChange = new EventEmitter<boolean>();
@@ -25,53 +26,73 @@ export class AddressDrawerComponent implements OnInit {
 
   @ViewChild('addressLabelInput') addressLabelInput!: ElementRef<HTMLInputElement>;
 
-  newAddress: Address = {
-    label: '',
-    address: '',
-    city: '',
-    zipCode: '',
-    phone: '',
-  };
+  addressForm: FormGroup;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private fb: FormBuilder) {
+    this.addressForm = this.fb.group({
+      label: ['', [Validators.required]],
+      address: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      zipCode: ['', [Validators.required]],
+      phone: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
+    this.loadInitialAddress();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['initialAddress']) {
+      this.loadInitialAddress();
+    }
+  }
+
+  private loadInitialAddress(): void {
     if (this.initialAddress) {
-      this.newAddress = { ...this.initialAddress };
+      setTimeout(() => {
+        this.addressForm.patchValue({
+          label: this.initialAddress!.label,
+          address: this.initialAddress!.address,
+          city: this.initialAddress!.city,
+          zipCode: this.initialAddress!.zipCode,
+          phone: this.initialAddress!.phone,
+        });
+      }, 0);
+    } else {
+      this.resetForm();
     }
   }
 
   closeDrawer(): void {
     this.visible = false;
     this.visibleChange.emit(false);
-    this.resetForm();
     this.closed.emit();
   }
 
   saveAddress(): void {
-    if (this.newAddress.address && this.newAddress.city) {
-      // Use DataService to save address
+    if (this.addressForm.valid) {
+      const formValue = this.addressForm.value;
+
       if (this.initialAddress?.id) {
-        // Update existing address
-        this.dataService.updateAddress({ ...this.newAddress, id: this.initialAddress.id });
+        this.dataService.updateAddress({ ...formValue, id: this.initialAddress.id });
       } else {
-        // Add new address
-        this.dataService.addAddress({ ...this.newAddress });
+        this.dataService.addAddress({ ...formValue });
       }
 
-      this.addressSaved.emit({ ...this.newAddress });
+      this.addressSaved.emit({ ...formValue });
       this.closeDrawer();
     }
   }
 
   private resetForm(): void {
-    this.newAddress = {
+    this.addressForm.reset({
       label: '',
       address: '',
       city: '',
       zipCode: '',
       phone: '',
-    };
+    });
   }
 
   focusInput(): void {
@@ -80,5 +101,21 @@ export class AddressDrawerComponent implements OnInit {
         this.addressLabelInput.nativeElement.focus();
       }, 100);
     }
+  }
+
+  // Helper methods for template
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.addressForm.get(fieldName);
+    return !!(field && field.invalid && (field.dirty || field.touched));
+  }
+
+  getFieldError(fieldName: string): string {
+    const field = this.addressForm.get(fieldName);
+    if (field?.errors && (field.dirty || field.touched)) {
+      if (field.errors['required']) {
+        return 'Это поле обязательно для заполнения';
+      }
+    }
+    return '';
   }
 }
